300da4042bc6d6f6ac7c51bb265c5c801770b44c
chotado <chotado.15@gmail.com>
2024-02-28 00:42:57 -0600

    redireccionar a boletines

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -1,18 +1,24 @@
+//Obtener los elementos de la clase .close
 let links = document.querySelectorAll(".close");
 
+//Recorrelos
 links.forEach(function(link){
+
+    //Agregar un evento click a cada de ellos - case sensitive
     link.addEventListener("click",function(ev){
         ev.preventDefault();
         let content = document.querySelector('.content');
 
+        //Quitarle las clases de animacion que ya tiene
         content.classList.remove("animate__fadeInDown");
         content.classList.remove("animate__animated");
 
+        //Agergar clases para animar su salida fadeOutUp
         content.classList.add("animate__animated");
         content.classList.add("animate__fadeOutUp");
 
         setTimeout(function(){
-            location.href = "/";
+            location.href = "/boletines";
         },600);
         
 
